555e69f7039df6a70bdf8ce9d061237c4652a00a
tankshali <tankshali@gmail.com>
2018-02-23 19:53:57 +0530

    mongo connection update

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ require('./models/models');
 var index = require('./routes/index');
 var api = require('./routes/api');
 var authenticate = require('./routes/authenticate')(passport);
-var mongoose = require('mongoose');    
+var mongoose = require('mongoose');
+var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
 var url = 'mongodb://admin:bi5CeQ5_fz6D@'||process.env.OPENSHIFT_MONGODB_DB_HOST||':'||process.env.OPENSHIFT_MONGODB_DB_PORT||'/fantasyleague';
-mongoose.connect('mongodb://admin:bi5CeQ5_fz6D@172.30.1.67:27017/fantasyleague',{auth:{authdb:"admin"}});            //connect to Mongo
+//mongoose.connect('mongodb://admin:bi5CeQ5_fz6D@172.30.1.67:27017/fantasyleague',{auth:{authdb:"admin"}});            //connect to Mongo
+mongoose.connect('mongodb://admin:bi5CeQ5_fz6D@'||mongoHost||':27017/fantasyleague',{auth:{authdb:"admin"}});
 var app = express();
 
 app.use(session({
@@ -37,8 +44,8 @@ app.use('/api', api);
 var initPassport = require('./passport-init');
 initPassport(passport);  
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
