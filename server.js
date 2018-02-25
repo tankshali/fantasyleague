71a10d7177227092f34e3978963f029e41556159
tankshali <tankshali@gmail.com>
2018-02-25 11:16:27 +0530

    mongo connection try

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
       mongoUser = process.env[mongoServiceName + '_USER'];
 var url = 'mongodb://admin:bi5CeQ5_fz6D@'||process.env.OPENSHIFT_MONGODB_DB_HOST||':'||process.env.OPENSHIFT_MONGODB_DB_PORT||'/fantasyleague';
-console.log('process.env.OPENSHIFT_MONGODB_DB_HOST - %s',process.env.OPENSHIFT_MONGODB_DB_HOST);
+//console.log('process.env.OPENSHIFT_MONGODB_DB_HOST - %s',process.env.OPENSHIFT_MONGODB_DB_HOST);
 //mongoose.connect('mongodb://admin:bi5CeQ5_fz6D@172.30.182.175:27017/fantasyleague',{auth:{authdb:"admin"}});//Cluster IP
 //mongoose.connect('mongodb://admin:bi5CeQ5_fz6D@10.129.158.152:27017/fantasyleague',{auth:{authdb:"admin"}});//Pod IP
 var conStr = 'mongodb://admin:bi5CeQ5_fz6D@'||process.env.OPENSHIFT_MONGODB_DB_HOST||':27017/fantasyleague';
@@ -51,10 +51,11 @@ initPassport(passport);
 
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('mongodbhost', process.env.OPENSHIFT_MONGODB_DB_HOST);
 
 http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
     console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
     console.log("process env test %s", process.env.PORT);
     console.log("process env test %s", process.env.OPENSHIFT_NODEJS_PORT);
-    console.log("process.env.OPENSHIFT_MONGODB_DB_HOST %s", process.env.OPENSHIFT_MONGODB_DB_HOST);
+    console.log("process.env.OPENSHIFT_MONGODB_DB_HOST %s", app.get('mongodbhost'));
 });
